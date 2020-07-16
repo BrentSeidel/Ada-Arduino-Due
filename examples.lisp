693a95aaa4029f777b39f3d78bdc050a20826988
BrentSeidel <brentseidel@mac.com>
2020-07-15 19:28:34 -0700

    Minor updates rebuilding with latest Lisp interpreter. Update example code to include dotimes operation.

--- examples.lisp
+++ examples.lisp
@@ -46,6 +46,12 @@
     (set-pca9685 pin n)
     (setq pin (+ pin 1))))
 ;
+;  Rewritten to use dotimes
+;
+(defun set-leds (n)
+  (dotimes (pin 8)
+    (set-pca9685 pin n)))
+;
 (defun cycle-leds (n)
   (setq count 0)
   (dowhile (< count n)
@@ -69,6 +75,26 @@
         (set-pca9685 pin value)
         (setq value (- value 100)))
       (setq pin (+ pin 1)))
+    (set-leds 0)))
+;
+;  Rewritten to use dotimes
+;
+(defun cycle-leds (n)
+  (dotimes (count n)
+    (print "Starting cycle " count)
+    (new-line)
+    (set-leds 0)
+    (dotimes (pin 8)
+      (setq value 0)
+      (dowhile (< value 4095)
+        (set-pca9685 pin value)
+        (setq value (+ value 100))))
+    (setq value 4095)
+    (dotimes (pin 8)
+      (setq value 4095)
+      (dowhile (> value 0)
+        (set-pca9685 pin value)
+        (setq value (- value 100))))
     (set-leds 0)))
 ;
 (setq pin 0)
